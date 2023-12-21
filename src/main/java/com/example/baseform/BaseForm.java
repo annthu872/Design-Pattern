2e8ce2cf0b27eaa02535833db347fc8c6655d4c9
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-12-21 12:23:09 +0700

    Update BaseForm.java

--- src/main/java/com/example/baseform/BaseForm.java
+++ src/main/java/com/example/baseform/BaseForm.java
@@ -1,15 +1,29 @@
 package com.example.baseform;
 
+import com.example.designpattern.Controller.PopupWindow;
+import com.example.tablehandler.TableGenFromDB;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 
 public class BaseForm<T> {
 	private T data;
 	
-	@FXML
-	private TableView myTable;
-	
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private TableView<?> myTable;
+    
     public BaseForm(T data) {
         this.data = data;
     }
@@ -18,7 +32,11 @@ public class BaseForm<T> {
     	 myTable.getColumns().clear();
     }
 	
-	void add() {};
-	void update() {};
+	void add() {
+		btnAdd.setOnAction(e -> PopupWindow.display(TableGenFromDB.getInstance().getColumnNames()));
+	};
+	void update() {
+		btnEdit.setOnAction(e -> PopupWindow.display(TableGenFromDB.getInstance().getColumnNames(),TableGenFromDB.getInstance().getFieldname()));
+	};
 	void delete() {};
 }
