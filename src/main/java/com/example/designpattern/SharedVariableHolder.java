1170801ca9362d939a350b94035598718697cb4d
HuynhNguyen0811 <81243529+HuynhNguyen0811@users.noreply.github.com>
2024-01-14 23:30:36 +0700

    add alert when input empty

--- src/main/java/com/example/designpattern/SharedVariableHolder.java
+++ src/main/java/com/example/designpattern/SharedVariableHolder.java
@@ -2,9 +2,9 @@ package com.example.designpattern;
 
 public class SharedVariableHolder {
     // Public static variable
-	public static String database = "sakila";
+	public static String database = "";
 	public static String url = "jdbc:mysql://localhost:3306/";
-	public static String user = "root";
-	public static String password = "192002";
+	public static String user = "";
+	public static String password = "";
 
 }
